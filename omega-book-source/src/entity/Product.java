00fcd2e55fd582791364985ded9075f7050c9863
Nguyen Thanh Canh <thanhcanh.dev@gmail.com>
2024-04-09 13:29:18 +0700

    Mapping entity nhóm sản phẩm

--- omega-book-source/src/entity/Product.java
+++ omega-book-source/src/entity/Product.java
@@ -4,201 +4,227 @@
  */
 package entity;
 
-import enums.Type;
-import enums.BookType;
 import java.util.Objects;
+import java.util.Set;
 import java.util.regex.Pattern;
+
+import enums.BookType;
+import enums.Type;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 /**
  *
  * @author thanhcanhit
  */
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Product {
 
-    //  Hằng số mô tả lỗi
-    public static final String PRODUCT_ID_INVALID = "Product id không hợp lệ";
-    public static final String NAME_EMPTY = "Product name không thể rỗng";
-    public static final String COST_PRICE_LOWER_ZERO = "Product costPrice không được bé hơn 0";
-    public static final String PRICE_INVALID = "Product price không được bé hơn 0 và bé hơn costPrice";
-    public static final String INVENTORY_LOWER_ZERO = "Product inventory không thể bé hơn 0";
-    public static final String VAT_LOWER_ZERO = "Product VAT không thể bé hơn 0";
-    protected String productID;
-    protected String name;
+	// Hằng số mô tả lỗi
+	public static final String PRODUCT_ID_INVALID = "Product id không hợp lệ";
+	public static final String NAME_EMPTY = "Product name không thể rỗng";
+	public static final String COST_PRICE_LOWER_ZERO = "Product costPrice không được bé hơn 0";
+	public static final String PRICE_INVALID = "Product price không được bé hơn 0 và bé hơn costPrice";
+	public static final String INVENTORY_LOWER_ZERO = "Product inventory không thể bé hơn 0";
+	public static final String VAT_LOWER_ZERO = "Product VAT không thể bé hơn 0";
+	@Id
+	protected String productID;
+	protected String name;
 
-protected Double costPrice;
-    protected byte[] image;
-    protected Double VAT;
-    protected Integer inventory;
-    protected Double price;
-    private Type type;
+	protected Double costPrice;
 
-    public Product() {
-    }
+	protected byte[] image;
+	protected Double VAT;
+	protected Integer inventory;
+	protected Double price;
 
-    public Product(String productID) throws Exception {
-        setProductID(productID);
-    }
+	@Enumerated(EnumType.ORDINAL)
+	private Type type;
 
-    public Product(String productID, String name, Double costPrice, byte[] image, Double VAT, Integer inventory, Type type) throws Exception {
-        setProductID(productID);
-        setName(name);
-        setCostPrice(costPrice);
-        setImage(image);
-        setVAT(VAT);
-        setInventory(inventory);
-        setType(type);
-        setPrice();
-    }
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "ProductPromotionDetail", joinColumns = @JoinColumn(name = "productID"), inverseJoinColumns = @JoinColumn(name = "promotionID"))
+	private Set<Promotion> promotions;
 
-    public Product(String productID, String name, Double costPrice, Double price, byte[] image, Double VAT, Integer inventory, Type type) throws Exception {
-        setProductID(productID);
-        setName(name);
-        setCostPrice(costPrice);
-        setImage(image);
-        setVAT(VAT);
-        setInventory(inventory);
-        setType(type);
-        this.price = price;
-    }
+	public Product() {
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Product other = (Product) obj;
-        return Objects.equals(this.productID, other.productID);
-    }
+	public Product(String productID) throws Exception {
+		setProductID(productID);
+	}
 
-    public Double getCostPrice() {
-        return costPrice;
-    }
+	public Product(String productID, String name, Double costPrice, byte[] image, Double VAT, Integer inventory,
+			Type type) throws Exception {
+		setProductID(productID);
+		setName(name);
+		setCostPrice(costPrice);
+		setImage(image);
+		setVAT(VAT);
+		setInventory(inventory);
+		setType(type);
+		setPrice();
+	}
 
-    public byte[] getImage() {
-        return image;
-    }
+	public Product(String productID, String name, Double costPrice, Double price, byte[] image, Double VAT,
+			Integer inventory, Type type) throws Exception {
+		setProductID(productID);
+		setName(name);
+		setCostPrice(costPrice);
+		setImage(image);
+		setVAT(VAT);
+		setInventory(inventory);
+		setType(type);
+		this.price = price;
+	}
 
-    public Integer getInventory() {
-        return inventory;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Product other = (Product) obj;
+		return Objects.equals(this.productID, other.productID);
+	}
 
-    public String getName() {
-        return name;
-    }
+	public Double getCostPrice() {
+		return costPrice;
+	}
 
-    public final Double getPrice() {
-        return this.price;
-    }
+	public byte[] getImage() {
+		return image;
+	}
 
-    public String getProductID() {
-        return productID;
-    }
+	public Integer getInventory() {
+		return inventory;
+	}
 
-    public Type getType() {
-        return type;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public Double getVAT() {
-        return VAT;
-    }
+	public final Double getPrice() {
+		return this.price;
+	}
 
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 53 * hash + Objects.hashCode(this.productID);
-        return hash;
-    }
+	public String getProductID() {
+		return productID;
+	}
 
-    public final void setCostPrice(Double costPrice) throws Exception {
-        if (costPrice < 0) {
-            throw new Exception(COST_PRICE_LOWER_ZERO);
-        }
-        this.costPrice = costPrice;
-    }
+	public Type getType() {
+		return type;
+	}
 
-    public final void setImage(byte[] image) {
-        this.image = image;
-    }
+	public Double getVAT() {
+		return VAT;
+	}
 
-    public final void setInventory(Integer inventory) throws Exception {
-        if (inventory < 0) {
-            throw new Exception(INVENTORY_LOWER_ZERO);
-        }
-        this.inventory = inventory;
-    }
+	@Override
+	public int hashCode() {
+		int hash = 5;
+		hash = 53 * hash + Objects.hashCode(this.productID);
+		return hash;
+	}
 
-    public final void setName(String name) throws Exception {
-        if (name.isBlank()) {
-            throw new Exception(NAME_EMPTY);
-        }
+	public final void setCostPrice(Double costPrice) throws Exception {
+		if (costPrice < 0) {
+			throw new Exception(COST_PRICE_LOWER_ZERO);
+		}
+		this.costPrice = costPrice;
+	}
 
-        this.name = name;
-    }
+	public final void setImage(byte[] image) {
+		this.image = image;
+	}
 
-    /**
-     * Hàm tính toán giá bán của sản phẩm dựa trên loại sản phẩm. Tỉ lệ được
-     * tính như sau: Sách 1.4; Sách ngoại văn 1.6; Văn phòng phẩm có giá vốn nhỏ
-     * hơn 20,000 1.6; Văn phòng phẩm có giá vốn lớn hơn 20,000: 1.5
-     *
-     */
-    private void setPrice() throws Exception {
-        Double rate = 1.0;
+	public final void setInventory(Integer inventory) throws Exception {
+		if (inventory < 0) {
+			throw new Exception(INVENTORY_LOWER_ZERO);
+		}
+		this.inventory = inventory;
+	}
+
+	public final void setName(String name) throws Exception {
+		if (name.isBlank()) {
+			throw new Exception(NAME_EMPTY);
+		}
+
+		this.name = name;
+	}
+
+	/**
+	 * Hàm tính toán giá bán của sản phẩm dựa trên loại sản phẩm. Tỉ lệ được tính
+	 * như sau: Sách 1.4; Sách ngoại văn 1.6; Văn phòng phẩm có giá vốn nhỏ hơn
+	 * 20,000 1.6; Văn phòng phẩm có giá vốn lớn hơn 20,000: 1.5
+	 *
+	 */
+	private void setPrice() throws Exception {
+		Double rate = 1.0;
 
 //      Xác định tỉ lệ lợi nhuận của sản phẩm
-        if (this.type == Type.BOOK) {
-            Book bookInstance = (Book) this;
-            if (bookInstance.getBookOrigin() == BookType.FOREIGN) {
-                rate = 1.6;
-            } else {
-                rate = 1.4;
-            }
-        } else if (this.type == Type.STATIONERY) {
-            if (costPrice < 20000) {
-                rate = 1.6;
-            } else {
-                rate = 1.5;
-            }
-        }
+		if (this.type == Type.BOOK) {
+			Book bookInstance = (Book) this;
+			if (bookInstance.getBookOrigin() == BookType.FOREIGN) {
+				rate = 1.6;
+			} else {
+				rate = 1.4;
+			}
+		} else if (this.type == Type.STATIONERY) {
+			if (costPrice < 20000) {
+				rate = 1.6;
+			} else {
+				rate = 1.5;
+			}
+		}
 
 //        Kiểm tra dữ liệu hợp lệ không
-        Double priceWithRate = costPrice * rate;
-        Double priceWithVAT = priceWithRate + (priceWithRate * VAT / 100);
-        if (priceWithVAT < 0 || priceWithVAT < costPrice) {
-            throw new Exception(PRICE_INVALID);
-        }
-        this.price = priceWithVAT;
-    }
+		Double priceWithRate = costPrice * rate;
+		Double priceWithVAT = priceWithRate + (priceWithRate * VAT / 100);
+		if (priceWithVAT < 0 || priceWithVAT < costPrice) {
+			throw new Exception(PRICE_INVALID);
+		}
+		this.price = priceWithVAT;
+	}
 
-    public void setProductID(String productID) throws Exception {
-        String regex = "^SP[1-3]\\d{2}\\d\\d{4}$";
-        Pattern pattern = Pattern.compile(regex);
+	public void setProductID(String productID) throws Exception {
+		String regex = "^SP[1-3]\\d{2}\\d\\d{4}$";
+		Pattern pattern = Pattern.compile(regex);
 
-        if (!pattern.matcher(productID).matches()) {
-            throw new Exception(PRODUCT_ID_INVALID);
-        }
-        this.productID = productID;
-    }
+		if (!pattern.matcher(productID).matches()) {
+			throw new Exception(PRODUCT_ID_INVALID);
+		}
+		this.productID = productID;
+	}
 
-    public final void setType(Type type) {
-        this.type = type;
-    }
+	public final void setType(Type type) {
+		this.type = type;
+	}
 
-    public final void setVAT(Double VAT) throws Exception {
-        if (VAT < 0) {
-            throw new Exception(VAT_LOWER_ZERO);
-        }
-        this.VAT = VAT;
-    }
+	public final void setVAT(Double VAT) throws Exception {
+		if (VAT < 0) {
+			throw new Exception(VAT_LOWER_ZERO);
+		}
+		this.VAT = VAT;
+	}
 
-    @Override
-    public String toString() {
-        return "Product{" + "productID=" + productID + ", name=" + name + ", costPrice=" + costPrice + ", image=" + image + ", VAT=" + VAT + ", inventory=" + inventory + ", price=" + price + ", type=" + type + '}';
-    }
+	@Override
+	public String toString() {
+		return "Product{" + "productID=" + productID + ", name=" + name + ", costPrice=" + costPrice + ", image="
+				+ image + ", VAT=" + VAT + ", inventory=" + inventory + ", price=" + price + ", type=" + type + '}';
+	}
 
 }
--- omega-book-source/src/entity/Promotion.java
+++ omega-book-source/src/entity/Promotion.java
@@ -1,179 +1,206 @@
 package entity;
 
-import enums.DiscountType;
-import enums.CustomerRank;
-import enums.PromotionType;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Objects;
+import java.util.Set;
 import java.util.regex.Pattern;
+
+import enums.CustomerRank;
+import enums.DiscountType;
+import enums.PromotionType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 /**
  *
  * @author Như Tâm
  */
+@Entity
 public final class Promotion {
-    public final String PROMOTIONID_ERROR="Mã chương trình khuyến mãi không hợp lệ!";
-    public final String STARTEDDATE_ERROR="Ngày tạo chương trình khuyến mãi không được rỗng!";
-    public final String ENDEDDATE_ERROR="Ngày kết thúc phải sau ngày tạo chương trình khuyến mãi!";
-    public final String DISCOUNT_ERROR="Giảm giá phải là số dương!";
-    
-    private String promotionID; 
-    private Date startedDate;
-    private Date endedDate;
-    private PromotionType typePromotion;
-    private DiscountType typeDiscount;
-    private double discount;
-    private CustomerRank condition;
-    private ArrayList<ProductPromotionDetail> listDetail;
+	public final String PROMOTIONID_ERROR = "Mã chương trình khuyến mãi không hợp lệ!";
+	public final String STARTEDDATE_ERROR = "Ngày tạo chương trình khuyến mãi không được rỗng!";
+	public final String ENDEDDATE_ERROR = "Ngày kết thúc phải sau ngày tạo chương trình khuyến mãi!";
+	public final String DISCOUNT_ERROR = "Giảm giá phải là số dương!";
 
-    public Promotion() {
-    }
-    //constructor theo mã khuyến mãi
-    public Promotion(String promotionID) throws Exception {
-        setPromotionID(promotionID);
-    }
-    public Promotion(String promotionID, Date startedDate, Date endedDate, PromotionType typePromotion, DiscountType typeDiscount, double discount) throws Exception {
-        setPromotionID(promotionID);
-        setStartedDate(startedDate);
-        setEndedDate(endedDate);
-        setTypePromotion(typePromotion);
-        setTypeDiscount(typeDiscount);
-        setDiscount(discount);
-    }
+	@Id
+	private String promotionID;
+	@Temporal(TemporalType.DATE)
+	private Date startedDate;
+	@Temporal(TemporalType.DATE)
+	private Date endedDate;
+	@Enumerated(EnumType.ORDINAL)
+	private PromotionType typePromotion;
+	@Enumerated(EnumType.ORDINAL)
+	private DiscountType typeDiscount;
+	private double discount;
+	@Enumerated(EnumType.ORDINAL)
+	private CustomerRank condition;
 
-    //constructor khuyến mãi theo sản phẩm
-    public Promotion(String promotionID, Date startedDate, Date endedDate, PromotionType typePromotion, DiscountType typeDiscount, double discount, ArrayList<ProductPromotionDetail> listDetail) throws Exception {
-        setPromotionID(promotionID);
-        setStartedDate(startedDate);
-        setEndedDate(endedDate);
-        setTypePromotion(typePromotion);
-        setTypeDiscount(typeDiscount);
-        setDiscount(discount);
-        setListDetail(listDetail);
-    }
-    
-    //constructor khuyến mãi theo hoá đơn
-    public Promotion(String promotionID, Date startedDate, Date endedDate, PromotionType typePromotion, DiscountType typeDiscount, double discount, CustomerRank condition) throws Exception {
-        setPromotionID(promotionID);
-        setStartedDate(startedDate);
-        setEndedDate(endedDate);
-        setTypePromotion(typePromotion);
-        setTypeDiscount(typeDiscount);
-        setDiscount(discount);
-        setCondition(condition);
-    }
+	@ManyToMany(mappedBy = "promotions", fetch = FetchType.LAZY)
+	private Set<Product> products;
 
-    //constructor đầy đủ
-    public Promotion(String promotionID, Date startedDate, Date endedDate, PromotionType typePromotion, DiscountType typeDiscount, double discount, CustomerRank condition, ArrayList<ProductPromotionDetail> listDetail) throws Exception {
-        setPromotionID(promotionID);
-        setStartedDate(startedDate);
-        setEndedDate(endedDate);
-        setTypePromotion(typePromotion);
-        setTypeDiscount(typeDiscount);
-        setDiscount(discount);
-        setCondition(condition);
-        setListDetail(listDetail);
-    }   
+	public Promotion() {
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Promotion other = (Promotion) obj;
-        return Objects.equals(this.promotionID, other.promotionID);
-    }
+	// constructor theo mã khuyến mãi
+	public Promotion(String promotionID) throws Exception {
+		setPromotionID(promotionID);
+	}
 
-    public CustomerRank getCondition() {
-        return condition;
-    }
+	public Promotion(String promotionID, Date startedDate, Date endedDate, PromotionType typePromotion,
+			DiscountType typeDiscount, double discount) throws Exception {
+		setPromotionID(promotionID);
+		setStartedDate(startedDate);
+		setEndedDate(endedDate);
+		setTypePromotion(typePromotion);
+		setTypeDiscount(typeDiscount);
+		setDiscount(discount);
+	}
 
-    public double getDiscount() {
-        return discount;
-    }
+	// constructor khuyến mãi theo sản phẩm
+	public Promotion(String promotionID, Date startedDate, Date endedDate, PromotionType typePromotion,
+			DiscountType typeDiscount, double discount, Set<Product> listDetail) throws Exception {
+		setPromotionID(promotionID);
+		setStartedDate(startedDate);
+		setEndedDate(endedDate);
+		setTypePromotion(typePromotion);
+		setTypeDiscount(typeDiscount);
+		setDiscount(discount);
+		setProducts(listDetail);
+	}
 
-    public Date getEndedDate() {
-        return endedDate;
-    }
+	// constructor khuyến mãi theo hoá đơn
+	public Promotion(String promotionID, Date startedDate, Date endedDate, PromotionType typePromotion,
+			DiscountType typeDiscount, double discount, CustomerRank condition) throws Exception {
+		setPromotionID(promotionID);
+		setStartedDate(startedDate);
+		setEndedDate(endedDate);
+		setTypePromotion(typePromotion);
+		setTypeDiscount(typeDiscount);
+		setDiscount(discount);
+		setCondition(condition);
+	}
 
-    public ArrayList<ProductPromotionDetail> getListDetail() {
-        return listDetail;
-    }
+	// constructor đầy đủ
+	public Promotion(String promotionID, Date startedDate, Date endedDate, PromotionType typePromotion,
+			DiscountType typeDiscount, double discount, CustomerRank condition, Set<Product> listDetail)
+			throws Exception {
+		setPromotionID(promotionID);
+		setStartedDate(startedDate);
+		setEndedDate(endedDate);
+		setTypePromotion(typePromotion);
+		setTypeDiscount(typeDiscount);
+		setDiscount(discount);
+		setCondition(condition);
+		setProducts(listDetail);
+	}
 
-    public String getPromotionID() {
-        return promotionID;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Promotion other = (Promotion) obj;
+		return Objects.equals(this.promotionID, other.promotionID);
+	}
 
-    public Date getStartedDate() {
-        return startedDate;
-    }
+	public CustomerRank getCondition() {
+		return condition;
+	}
 
-    public DiscountType getTypeDiscount() {
-        return typeDiscount;
-    }
+	public double getDiscount() {
+		return discount;
+	}
 
-    public PromotionType getTypePromotion() {
-        return typePromotion;
-    }
+	public Date getEndedDate() {
+		return endedDate;
+	}
 
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 37 * hash + Objects.hashCode(this.promotionID);
-        return hash;
-    }
+	public String getPromotionID() {
+		return promotionID;
+	}
 
-    public void setCondition(CustomerRank condition) {
-        this.condition = condition;
-    }
+	public Date getStartedDate() {
+		return startedDate;
+	}
 
-    public void setDiscount(double discount) {
-        this.discount = discount;
-    }
+	public DiscountType getTypeDiscount() {
+		return typeDiscount;
+	}
 
-    public void setEndedDate(Date endedDate) throws Exception {
-        if(endedDate.before(startedDate))
-            throw new Exception(ENDEDDATE_ERROR);
-        this.endedDate = endedDate;
-    }
+	public PromotionType getTypePromotion() {
+		return typePromotion;
+	}
 
-    public void setListDetail(ArrayList<ProductPromotionDetail> listDetail) {
-        this.listDetail = listDetail;
-    }
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		hash = 37 * hash + Objects.hashCode(this.promotionID);
+		return hash;
+	}
 
-    public void setPromotionID(String promotionID) throws Exception {
-        String pattern = "^(KM)[0-9]{1}[0-9]{1}[0-9]{8}[0-9]{4}$";
-        if(!Pattern.matches(pattern, promotionID))
-            throw new Exception(PROMOTIONID_ERROR);
-        this.promotionID = promotionID;
-    }
+	public void setCondition(CustomerRank condition) {
+		this.condition = condition;
+	}
 
-    public void setStartedDate(Date startedDate) {
-        this.startedDate = startedDate;
-    }
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
 
-    public void setTypeDiscount(DiscountType typeDiscount) throws Exception {
-        if(discount < 0)
-            throw new Exception(DISCOUNT_ERROR);
-        this.typeDiscount = typeDiscount;
-    }
+	public void setEndedDate(Date endedDate) throws Exception {
+		if (endedDate.before(startedDate))
+			throw new Exception(ENDEDDATE_ERROR);
+		this.endedDate = endedDate;
+	}
 
-    public void setTypePromotion(PromotionType typePromotion) {
-        this.typePromotion = typePromotion;
-    }
+	public void setPromotionID(String promotionID) throws Exception {
+		String pattern = "^(KM)[0-9]{1}[0-9]{1}[0-9]{8}[0-9]{4}$";
+		if (!Pattern.matches(pattern, promotionID))
+			throw new Exception(PROMOTIONID_ERROR);
+		this.promotionID = promotionID;
+	}
 
-    
-    @Override
-    public String toString() {
-        return "Promotion{" + "PROMOTIONID_ERROR=" + PROMOTIONID_ERROR + ", STARTEDDATE_ERROR=" + STARTEDDATE_ERROR + ", ENDEDDATE_ERROR=" + ENDEDDATE_ERROR + ", DISCOUNT_ERROR=" + DISCOUNT_ERROR + ", promotionID=" + promotionID + ", startedDate=" + startedDate + ", endedDate=" + endedDate + ", typePromotion=" + typePromotion + ", typeDiscount=" + typeDiscount + ", discount=" + discount + ", condition=" + condition + ", listDetail=" + listDetail + '}';
-    }
-    
-    
+	public void setStartedDate(Date startedDate) {
+		this.startedDate = startedDate;
+	}
+
+	public void setTypeDiscount(DiscountType typeDiscount) throws Exception {
+		if (discount < 0)
+			throw new Exception(DISCOUNT_ERROR);
+		this.typeDiscount = typeDiscount;
+	}
+
+	public void setTypePromotion(PromotionType typePromotion) {
+		this.typePromotion = typePromotion;
+	}
+
+	@Override
+	public String toString() {
+		return "Promotion{" + "PROMOTIONID_ERROR=" + PROMOTIONID_ERROR + ", STARTEDDATE_ERROR=" + STARTEDDATE_ERROR
+				+ ", ENDEDDATE_ERROR=" + ENDEDDATE_ERROR + ", DISCOUNT_ERROR=" + DISCOUNT_ERROR + ", promotionID="
+				+ promotionID + ", startedDate=" + startedDate + ", endedDate=" + endedDate + ", typePromotion="
+				+ typePromotion + ", typeDiscount=" + typeDiscount + ", discount=" + discount + ", condition="
+				+ condition;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+
 }
