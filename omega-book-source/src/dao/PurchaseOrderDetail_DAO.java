8e1bc2e8d553349d65e567356d601e62c98aa5c9
Lê Hoàng Khang <iamhoangkhang@icloud.com>
2024-04-10 16:11:31 +0700

    Cập nhật DAO (Sử dụng HQL)

--- omega-book-source/src/dao/PurchaseOrderDetail_DAO.java
+++ omega-book-source/src/dao/PurchaseOrderDetail_DAO.java
@@ -6,6 +6,9 @@ package dao;
 
 import entity.PurchaseOrderDetail;
 import interfaces.DAOBase;
+import jakarta.persistence.EntityManager;
+import utilities.AccessDatabase;
+
 import java.util.ArrayList;
 import database.ConnectDB;
 import entity.Product;
@@ -14,51 +17,59 @@ import java.sql.*;
 
 /**
  *
- * @author KienTran
+ * @author KienTran + QUuanfKhang đẹp tright
  */
 public class PurchaseOrderDetail_DAO implements DAOBase<PurchaseOrderDetail>{
 
-    @Override
+	EntityManager entityManager;
+	
+    public PurchaseOrderDetail_DAO() {
+		entityManager = AccessDatabase.getEntityManager();
+	}
+
+	@Override
     public PurchaseOrderDetail getOne(String id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
     public ArrayList<PurchaseOrderDetail> getAll() {
-         ArrayList<PurchaseOrderDetail> result = new ArrayList<>();
-        try {
-            Statement st = ConnectDB.conn.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM PurchaseOrderDetail;");
-            while (rs.next()) {
-                String id = rs.getString("purchaseOrderID");
-                String productID = rs.getString("productID");
-                int quantity = rs.getInt("Quantity");
-                Double costPrice = rs.getDouble("costPrice");
-                result.add(new PurchaseOrderDetail(new PurchaseOrder(id), new Product(productID), quantity, costPrice, quantity*costPrice));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
+//         ArrayList<PurchaseOrderDetail> result = new ArrayList<>();
+//        try {
+//            Statement st = ConnectDB.conn.createStatement();
+//            ResultSet rs = st.executeQuery("SELECT * FROM PurchaseOrderDetail;");
+//            while (rs.next()) {
+//                String id = rs.getString("purchaseOrderID");
+//                String productID = rs.getString("productID");
+//                int quantity = rs.getInt("Quantity");
+//                Double costPrice = rs.getDouble("costPrice");
+//                result.add(new PurchaseOrderDetail(new PurchaseOrder(id), new Product(productID), quantity, costPrice, quantity*costPrice));
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return result;
+    	return (ArrayList<PurchaseOrderDetail>) entityManager.createQuery("SELECT pod FROM PurchaseOrderDetail pod", PurchaseOrderDetail.class).getResultList();
     }
     public ArrayList<PurchaseOrderDetail> getAll(String id) {
-        ArrayList<PurchaseOrderDetail> result = new ArrayList<>();
-        try{
-            PreparedStatement st = ConnectDB.conn.prepareStatement("Select * from PurchaseOrderDetail where PurchaseOrderID= ?");
-            st.setString(1, id);
-            ResultSet rs = st.executeQuery();
-            while(rs.next()){
-                String productID = rs.getString("productID");
-                int quantity = rs.getInt("Quantity");
-                Double costPrice = rs.getDouble("costPrice");
-                result.add(new PurchaseOrderDetail(new PurchaseOrder(id), new Product_DAO().getOne(productID), quantity, costPrice, quantity*costPrice));
-            }
-                   
-        }catch(Exception e){
-         e.printStackTrace();
-        }
-        return result;
-        
+//        ArrayList<PurchaseOrderDetail> result = new ArrayList<>();
+//        try{
+//            PreparedStatement st = ConnectDB.conn.prepareStatement("Select * from PurchaseOrderDetail where PurchaseOrderID= ?");
+//            st.setString(1, id);
+//            ResultSet rs = st.executeQuery();
+//            while(rs.next()){
+//                String productID = rs.getString("productID");
+//                int quantity = rs.getInt("Quantity");
+//                Double costPrice = rs.getDouble("costPrice");
+//                result.add(new PurchaseOrderDetail(new PurchaseOrder(id), new Product_DAO().getOne(productID), quantity, costPrice, quantity*costPrice));
+//            }
+//                   
+//        }catch(Exception e){
+//         e.printStackTrace();
+//        }
+//        return result;
+    	
+    	return (ArrayList<PurchaseOrderDetail>) entityManager.createQuery("SELECT pod FROM PurchaseOrderDetail pod WHERE pod.purchaseOrder.purchaseOrderID = :id", PurchaseOrderDetail.class).setParameter("id", id).getResultList();
     }
 
     @Override
@@ -66,23 +77,37 @@ public class PurchaseOrderDetail_DAO implements DAOBase<PurchaseOrderDetail>{
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    
     @Override
+    /**
+     * Thêm mới một chi tiết đơn đặt hàng vào cơ sở dữ liệu
+     */
     public Boolean create(PurchaseOrderDetail object) {
-        int n = 0;
-
-        try {
-            PreparedStatement st = ConnectDB.conn.prepareStatement("INSERT INTO PurchaseOrderDetail(purchaseOrderID, productID, quantity, costPrice) VALUES (?, ?, ?, ?);");
-            st.setString(1, object.getPurchaseOrder().getPurchaseOrderID());
-            st.setString(2, object.getProduct().getProductID());
-            st.setInt(3, object.getQuantity()); 
-            st.setDouble(4, object.getCostPrice());
-          
-            n = st.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return n > 0;
+//        int n = 0;
+//
+//        try {
+//            PreparedStatement st = ConnectDB.conn.prepareStatement("INSERT INTO PurchaseOrderDetail(purchaseOrderID, productID, quantity, costPrice) VALUES (?, ?, ?, ?);");
+//            st.setString(1, object.getPurchaseOrder().getPurchaseOrderID());
+//            st.setString(2, object.getProduct().getProductID());
+//            st.setInt(3, object.getQuantity()); 
+//            st.setDouble(4, object.getCostPrice());
+//          
+//            n = st.executeUpdate();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        return n > 0;
+    	try {
+    		entityManager.getTransaction().begin();
+    		entityManager.persist(object);
+    		entityManager.getTransaction().commit();
+    		return true;
+    	}
+		catch (Exception e) {
+//			e.printStackTrace();
+			return false;
+		}
     }
 
     @Override
@@ -92,14 +117,26 @@ public class PurchaseOrderDetail_DAO implements DAOBase<PurchaseOrderDetail>{
 
     @Override
     public Boolean delete(String id) {
-        int n = 0;
-        try {
-            PreparedStatement st = ConnectDB.conn.prepareStatement("delete from PurchaseOrderDetail where PurchaseOrderID = ?");
-            st.setString(1, id);
-            n = st.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
+//        int n = 0;
+//        try {
+//            PreparedStatement st = ConnectDB.conn.prepareStatement("delete from PurchaseOrderDetail where PurchaseOrderID = ?");
+//            st.setString(1, id);
+//            n = st.executeUpdate();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return n > 0;}
+    	
+    	try {
+    		entityManager.getTransaction().begin();
+    		entityManager.remove(entityManager.find(PurchaseOrderDetail.class, id));
+    		entityManager.getTransaction().commit();
+    		return true;
+    	}
+        catch (Exception e) {
+			// e.printStackTrace();
+			return false;
+		}
         }
-        return n > 0;}
     
 }
