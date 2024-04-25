4ad2836a06cb1943492c6a903f401832849d6d33
kientrank3 <bankienthanthien@gmail.com>
2024-04-25 10:18:20 +0700

    Update Sales_BUSImpl

--- omega-book-client/src/main/java/main/Application.java
+++ omega-book-client/src/main/java/main/Application.java
@@ -197,8 +197,8 @@ public class Application extends javax.swing.JFrame {
 
 	public static void main(String args[]) throws MalformedURLException, NotBoundException {
 		//		SET PORT & URL SERVICE
-		//RMIService.setPU(7878, "rmi://192.168.0.160:");
-		RMIService.setPU(7878, "rmi://localhost:");
+		RMIService.setPU(7878, "rmi://172.20.33.226:");
+//		RMIService.setPU(7878, "rmi://localhost:");
 
 		FlatRobotoFont.install();
 		FlatLaf.registerCustomDefaultsSource("theme");
